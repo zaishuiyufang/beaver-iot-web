dcf15c8e8c08a92c80503624998a120132f52e3c
谢建聪 <xiejc@milesight.com>
2024-09-26 15:12:12 +0800

    feat: 新增通用 copy 逻辑

---
+++ packages/shared/src/hooks/useCopy.tsx
@@ -0,0 +1,25 @@
+import { useCallback } from 'react';
+import { toast } from '../components';
+import useI18n from './useI18n';
+import { copyText } from '../utils/clipboard';
+
+/**
+ * 通用文案复制逻辑
+ */
+const useCopy = () => {
+    const { getIntlText } = useI18n();
+    const handleCopy = useCallback(
+        async (text: string) => {
+            if (!text) return;
+            const res = await copyText(text);
+            res && toast.success({ content: getIntlText('common.message.copy_successful') });
+        },
+        [getIntlText],
+    );
+
+    return {
+        handleCopy,
+    };
+};
+
+export default useCopy;
