70d457142f957e9f6bdb6e97b31a92c202856c18
zhuangjh <zhuangjh@milesight.com>
2024-10-09 10:33:22 +0800

    feat: 插件文件增加自动导出逻辑

--- apps/web/src/plugin/plugins/components.ts
+++ apps/web/src/plugin/plugins/components.ts
@@ -1,1 +1,16 @@
-export default ['trigger', 'demand-analysis'];
+import { isFileName } from '@milesight/shared/src/utils/tools';
+
+/** 提取子文件夹名 */
+const getFolderName = (modules: ModuleType) => {
+    return Object.keys(modules).reduce((bucket, path) => {
+        const [, folder] = path?.split('/') || [];
+        if (!folder || bucket.includes(folder) || isFileName(folder)) return bucket;
+
+        bucket.push(folder);
+        return bucket;
+    }, [] as string[]);
+};
+
+const modules = import.meta.glob('../plugins/**');
+const folderNames = getFolderName(modules);
+export default folderNames;
--- apps/web/src/plugin/plugins/index.ts
+++ apps/web/src/plugin/plugins/index.ts
@@ -1,2 +1,26 @@
-export { default as demandAnalysisConfig } from './demand-analysis/configure';
-export { default as demandAnalysisView } from './demand-analysis/view';
+import { camelCase } from 'lodash-es';
+import { isFileName } from '@milesight/shared/src/utils/tools';
+
+const getModuleFiles = async (modules: ModuleType, suffix?: string) => {
+    let bucket = {};
+    for await (const path of Object.keys(modules)) {
+        const module = await modules[path]();
+
+        const component = module.default;
+        const [, folder] = path?.split('/') || [];
+        if (!folder || folder in bucket || isFileName(folder)) continue;
+
+        const name = camelCase(folder) + (suffix || '');
+        bucket = { ...bucket, [name]: component };
+    }
+    return bucket;
+};
+
+const configureModules = import.meta.glob('../plugins/*/configure/index.tsx');
+const viewModules = import.meta.glob('../plugins/*/view/index.tsx');
+
+const configFiles = await getModuleFiles(configureModules, 'Config');
+const viewFiles = await getModuleFiles(viewModules, 'View');
+const modules = { ...configFiles, ...viewFiles };
+
+export default modules;
