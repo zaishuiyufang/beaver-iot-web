f0333d749c8dbca9789ad1ee37b3e6636c5eaf2e
谢建聪 <xiejc@milesight.com>
2024-10-12 11:21:54 +0800

    fix: Setting 页面布局及样式调整

--- apps/web/src/pages/setting/index.tsx
+++ apps/web/src/pages/setting/index.tsx
@@ -17,19 +17,11 @@ function App() {
             <Breadcrumbs />
             <div className="ms-view ms-view-setting">
                 <Tabs className="ms-tabs" value={tab} onChange={handleChange}>
-                    <Tab disableRipple title="Item One" label="Item One" value="1" />
-                    <Tab disableRipple title="Item Two" label="Item Two" value="2" />
-                    <Tab disableRipple title="Item Three" label="Item Three" value="3" />
+                    <Tab disableRipple title="Integration" label="Integration" value="1" />
                 </Tabs>
                 <div className="ms-tab-content">
                     <TabPanel value={tab} index="1">
                         <Integration />
-                    </TabPanel>
-                    <TabPanel value={tab} index="2">
-                        Item 222
-                    </TabPanel>
-                    <TabPanel value={tab} index="3">
-                        Item 333
                     </TabPanel>
                 </div>
             </div>
