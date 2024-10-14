db840738240122bf74a68600666aa0aa20ac3b5c
zhuangjh <zhuangjh@milesight.com>
2024-10-14 13:58:00 +0800

    feat: 处理组件点击报错问题

--- apps/web/src/plugin/plugins/data-card/configure/index.tsx
+++ apps/web/src/plugin/plugins/data-card/configure/index.tsx
@@ -3,18 +3,27 @@ import { RenderConfig } from '../../../render';
 import config from '../config.json';
 
 interface ConfigPluginProps {
+    value: any;
     onOk: (data: any) => void;
     onChange: (data: any) => void;
 }
 
 const Plugin = forwardRef((props: ConfigPluginProps, ref: any) => {
-    const { onOk, onChange } = props;
+    const { value, onOk, onChange } = props;
 
     const handleSubmit = (data: any) => {
         onOk(data);
     };
 
-    return <RenderConfig config={config} onOk={handleSubmit} ref={ref} onChange={onChange} />;
+    return (
+        <RenderConfig
+            config={config}
+            onOk={handleSubmit}
+            ref={ref}
+            value={value}
+            onChange={onChange}
+        />
+    );
 });
 
 export default Plugin;
--- apps/web/src/plugin/plugins/gauge-chart/configure/index.tsx
+++ apps/web/src/plugin/plugins/gauge-chart/configure/index.tsx
@@ -3,17 +3,18 @@ import { RenderConfig } from '../../../render';
 import config from '../config.json';
 
 interface ConfigPluginProps {
+    value: any;
     onOk: (data: any) => void;
 }
 
 const Plugin = forwardRef((props: ConfigPluginProps, ref: any) => {
-    const { onOk } = props;
+    const { value, onOk } = props;
 
     const handleSubmit = (data: any) => {
         onOk(data);
     };
 
-    return <RenderConfig config={config} onOk={handleSubmit} ref={ref} />;
+    return <RenderConfig config={config} onOk={handleSubmit} ref={ref} value={value} />;
 });
 
 export default Plugin;
