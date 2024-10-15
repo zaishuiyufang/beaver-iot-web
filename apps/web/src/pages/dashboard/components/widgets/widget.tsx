ee13bd1435a985f2fac766e284e8633008e49160
linwb <a120059397@qq.com>
2024-10-15 14:26:49 +0800

    feat: dashboard增加窗口变化的时候按照比例计算位置，以及组件大小按照24等分计算

--- apps/web/src/pages/dashboard/components/widgets/widget.tsx
+++ apps/web/src/pages/dashboard/components/widgets/widget.tsx
@@ -6,7 +6,7 @@ import { RenderView } from '@/plugin/render';
 
 interface WidgetProps {
     data: any;
-    onResizeBox: ({ id, width, height, initWidth, initHeight }: draggerType) => void;
+    onResizeBox: (data: draggerType) => void;
     onMove: ({ id, left, top }: { id?: string; left: number; top: number }) => void;
     isEdit: boolean;
     onEdit: (data: any) => void;
@@ -20,15 +20,30 @@ const Widget = (props: WidgetProps) => {
     const widgetRef = useRef<HTMLDivElement>(null);
     const [pos, setPos] = useState<draggerType>();
 
+    const getSize = () => {
+        // 计算出24等分一格大小
+        const unitHeight = (parentRef?.current?.clientHeight || 0) / 24;
+        const unitWidth = (parentRef?.current?.clientWidth || 0) / 24;
+        // 计算当前宽度跟高度各占几格并向上取整
+        const widthCol = Math.ceil((widgetRef?.current?.clientWidth || 0) / unitWidth);
+        const heightRow = Math.ceil((widgetRef?.current?.clientHeight || 0) / unitHeight);
+        return {
+            widthCol,
+            heightRow,
+            unitHeight,
+            unitWidth,
+        };
+    };
+
     useEffect(() => {
         if (!data?.pos?.width && !data?.pos?.height && widgetRef?.current) {
-            setPos({
-                ...(data.pos || {}),
-                initWidth: widgetRef?.current?.clientWidth,
-                initHeight: widgetRef?.current?.clientHeight,
-                width: widgetRef?.current?.clientWidth,
-                height: widgetRef?.current?.clientHeight,
-            });
+            // setPos({
+            //     ...(data.pos || {}),
+            //     width,
+            //     height,
+            //     initWidth: widgetRef?.current?.clientWidth,
+            //     initHeight: widgetRef?.current?.clientHeight,
+            // });
             onResizeBox({
                 id: data.id,
                 width: widgetRef?.current?.clientWidth,
@@ -37,8 +52,11 @@ const Widget = (props: WidgetProps) => {
                 initHeight: widgetRef?.current?.clientHeight,
             });
         } else {
+            const { unitHeight, unitWidth } = getSize();
             setPos({
                 ...(data.pos || {}),
+                width: (data?.pos?.width || 0) * unitWidth,
+                height: (data?.pos?.height || 0) * unitHeight,
             });
         }
     }, [data]);
