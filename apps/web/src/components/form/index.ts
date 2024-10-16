02c1c805ce47e6c9b1101e3a8b920bc6a234fc56
谢建聪 <xiejc@milesight.com>
2024-10-16 19:18:56 +0800

    fix: 移除冗余组件

--- apps/web/src/components/form/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as FormInput, type FormInputProps } from './form-input';
-export { default as FormRadio, type FormRadioProps } from './form-radio';
-export { default as FormSelect, type FormSelectProps } from './form-select';
