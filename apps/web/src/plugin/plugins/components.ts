56732753114c9b84796dc3afe0861658f83d31dc
zhuangjh <zhuangjh@milesight.com>
2024-10-17 11:34:14 +0800

    fix: 修复插件自动导入的编译报错

--- apps/web/src/plugin/plugins/components.ts
+++ apps/web/src/plugin/plugins/components.ts
@@ -1,16 +1,19 @@
 import { isFileName } from '@milesight/shared/src/utils/tools';
 
-/** 提取子文件夹名 */
-const getFolderName = (modules: ModuleType) => {
-    return Object.keys(modules).reduce((bucket, path) => {
-        const [, folder] = path?.split('/') || [];
-        if (!folder || bucket.includes(folder) || isFileName(folder)) return bucket;
+const folderNames = (() => {
+    /** 提取子文件夹名 */
+    const getFolderName = (modules: ModuleType) => {
+        return Object.keys(modules).reduce((bucket, path) => {
+            const [, folder] = path?.split('/') || [];
+            if (!folder || bucket.includes(folder) || isFileName(folder)) return bucket;
 
-        bucket.push(folder);
-        return bucket;
-    }, [] as string[]);
-};
+            bucket.push(folder);
+            return bucket;
+        }, [] as string[]);
+    };
 
-const modules = import.meta.glob('../plugins/**');
-const folderNames = getFolderName(modules);
+    const modules = import.meta.glob('../plugins/**');
+    return getFolderName(modules);
+})();
+
 export default folderNames;
--- apps/web/src/plugin/plugins/index.ts
+++ apps/web/src/plugin/plugins/index.ts
@@ -1,26 +1,28 @@
 import { camelCase } from 'lodash-es';
 import { isFileName } from '@milesight/shared/src/utils/tools';
 
-const getModuleFiles = async (modules: ModuleType, suffix?: string) => {
-    let bucket = {};
-    for await (const path of Object.keys(modules)) {
-        const module = await modules[path]();
+const modules = (async () => {
+    const getModuleFiles = async (modules: ModuleType, suffix?: string) => {
+        let bucket = {};
+        for await (const path of Object.keys(modules)) {
+            const module = await modules[path]();
 
-        const component = module.default;
-        const [, folder] = path?.split('/') || [];
-        if (!folder || folder in bucket || isFileName(folder)) continue;
+            const component = module.default;
+            const [, folder] = path?.split('/') || [];
+            if (!folder || folder in bucket || isFileName(folder)) continue;
 
-        const name = camelCase(folder) + (suffix || '');
-        bucket = { ...bucket, [name]: component };
-    }
-    return bucket;
-};
+            const name = camelCase(folder) + (suffix || '');
+            bucket = { ...bucket, [name]: component };
+        }
+        return bucket;
+    };
 
-const configureModules = import.meta.glob('../plugins/*/configure/index.tsx');
-const viewModules = import.meta.glob('../plugins/*/view/index.tsx');
+    const configureModules = import.meta.glob('../plugins/*/configure/index.tsx');
+    const viewModules = import.meta.glob('../plugins/*/view/index.tsx');
 
-const configFiles = await getModuleFiles(configureModules, 'Config');
-const viewFiles = await getModuleFiles(viewModules, 'View');
-const modules = { ...configFiles, ...viewFiles };
+    const configFiles = await getModuleFiles(configureModules, 'Config');
+    const viewFiles = await getModuleFiles(viewModules, 'View');
+    return { ...configFiles, ...viewFiles };
+})();
 
 export default modules;
