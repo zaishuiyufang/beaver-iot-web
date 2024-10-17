ee85f7c0e57815ec949b0e2f2b66401344a3bb27
谢建聪 <xiejc@milesight.com>
2024-10-17 17:10:25 +0800

    fix: 404 页面样式优化调整

--- apps/web/src/pages/404/index.tsx
+++ apps/web/src/pages/404/index.tsx
@@ -1,8 +1,14 @@
 import { Typography } from '@mui/material';
+import { useI18n } from '@milesight/shared/src/hooks';
+import './style.less';
 
-export default () => (
-    <>
-        <Typography variant="h3">404</Typography>
-        <Typography>Sorry, the page you visited does not exist.</Typography>
-    </>
-);
+export default () => {
+    const { getIntlText } = useI18n();
+
+    return (
+        <div className="ms-view-404">
+            <Typography variant="h2">404</Typography>
+            <Typography>{getIntlText('error.http.page_not_found')}</Typography>
+        </div>
+    );
+};
