d0931c5ee3168772547f1b50de65ad788f9b0db2
linwb <a120059397@qq.com>
2024-10-31 17:10:07 +0800

    feat: dashboard增加饼图组件

---
+++ apps/web/src/plugin/plugins/pie-chart/typings.d.ts
@@ -0,0 +1,8 @@
+export interface ViewConfigProps {
+    title: string;
+    entity: EntityOptionType;
+    time: number;
+    metrics: DataAggregateType;
+}
+
+export type ConfigureType = any;
---
+++ apps/web/src/plugin/plugins/pie-chart/view/index.tsx
@@ -0,0 +1,111 @@
+import { useEffect, useRef } from 'react';
+import { useRequest } from 'ahooks';
+import Chart, { ChartConfiguration } from 'chart.js/auto'; // 引入 Chart.js
+import { useI18n, useTheme } from '@milesight/shared/src/hooks';
+import {
+    awaitWrap,
+    entityAPI,
+    EntityAPISchema,
+    getResponseData,
+    isRequestSuccess,
+} from '@/services/http';
+import { ViewConfigProps } from '../typings';
+import './style.less';
+
+interface IProps {
+    config: ViewConfigProps;
+}
+interface AggregateHistoryList {
+    entity: EntityOptionType;
+    data: EntityAPISchema['getAggregateHistory']['response'];
+}
+const View = (props: IProps) => {
+    const { config } = props;
+    const { entity, title, metrics, time } = config || {};
+    const { getIntlText } = useI18n();
+    const { blue, white } = useTheme();
+    const headerLabel = title || getIntlText('common.label.title');
+
+    const chartRef = useRef<HTMLCanvasElement>(null);
+    const { data: aggregateHistoryList } = useRequest(
+        async () => {
+            if (!entity?.value) return;
+
+            const run = async (selectEntity: EntityOptionType) => {
+                const { value: entityId } = selectEntity || {};
+                if (!entityId) return;
+
+                const now = Date.now();
+                const [error, resp] = await awaitWrap(
+                    entityAPI.getAggregateHistory({
+                        entity_id: entityId,
+                        aggregate_type: metrics,
+                        start_timestamp: now - time,
+                        end_timestamp: now,
+                    }),
+                );
+                if (error || !isRequestSuccess(resp)) return;
+
+                const data = getResponseData(resp);
+                return {
+                    entity,
+                    data,
+                } as AggregateHistoryList;
+            };
+            return Promise.resolve(run(entity));
+        },
+        { refreshDeps: [entity, time, metrics] },
+    );
+
+    /** 渲染雷达图 */
+    const renderRadarChart = () => {
+        const ctx = chartRef.current!;
+        const data = aggregateHistoryList?.data?.count_result || [];
+        if (!ctx || !data?.length) return;
+        const chart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: data?.map(item => item.value), // 数据标签
+                datasets: [
+                    {
+                        // label: 'My First Dataset',
+                        data: data?.map(item => item.count) as any, // 数据值, // 数据值
+                        borderWidth: 1, // 边框宽度
+                    },
+                ],
+            },
+            options: {
+                responsive: true, // 使图表响应式
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        position: 'right', // 图例位置
+                    },
+                    tooltip: {
+                        enabled: true, // 启用提示工具
+                    },
+                },
+            },
+        });
+        return () => {
+            /**
+             * 清空图表数据
+             */
+            chart.destroy();
+        };
+    };
+    useEffect(() => {
+        return renderRadarChart();
+    }, [aggregateHistoryList]);
+
+    return (
+        <div className="ms-radar-chart">
+            <div className="ms-radar-chart__header">{headerLabel}</div>
+            <div className="ms-radar-chart__content">
+                <canvas id="radarChart" ref={chartRef} />
+            </div>
+        </div>
+    );
+};
+
+export default View;
