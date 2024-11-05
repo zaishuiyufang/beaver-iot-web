c759254e2fc4202fb0363ae913fa9372d19fa12a
linwb <a120059397@qq.com>
2024-11-05 21:18:25 +0800

    feat(dashboard): 颜色选择器增加确认按钮

--- apps/web/src/plugin/components/icon-color-select/index.tsx
+++ apps/web/src/plugin/components/icon-color-select/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-import { Box, MenuItem } from '@mui/material';
+import { Box, MenuItem, Button } from '@mui/material';
 import { SketchPicker } from 'react-color';
+import { useI18n } from '@milesight/shared/src/hooks';
 import Select from '../select';
 import './style.less';
 
 const IconColorSelect = (props: any) => {
+    const { getIntlText } = useI18n();
     const { value, onChange, ...rest } = props;
     const [open, setOpen] = useState(false);
 
@@ -13,7 +15,7 @@ const IconColorSelect = (props: any) => {
 
     const handleColorChange = (color: any) => {
         onChange(color.hex);
-        handleClose();
+        // handleClose();
     };
 
     return (
@@ -35,6 +37,13 @@ const IconColorSelect = (props: any) => {
                     {rest.value}
                 </Box>
             )}
+            MenuProps={{
+                MenuListProps: {
+                    sx: {
+                        padding: 0,
+                    },
+                },
+            }}
             renderOptions={() => {
                 return (
                     <MenuItem onClick={handleOpen} className="icon-color-select-menu">
@@ -48,6 +57,16 @@ const IconColorSelect = (props: any) => {
                                 color={value}
                                 onChangeComplete={handleColorChange}
                             />
+                            <div className="icon-color-select-submit">
+                                <Button
+                                    className="icon-color-select-button"
+                                    onClick={handleClose}
+                                    fullWidth
+                                    disableRipple
+                                >
+                                    {getIntlText('common.button.confirm')}
+                                </Button>
+                            </div>
                         </div>
                     </MenuItem>
                 );
