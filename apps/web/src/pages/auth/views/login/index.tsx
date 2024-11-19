3b286bab89e4d542faf49933ed37c67fd853b0a7
谢建聪 <xiejc@milesight.com>
2024-11-19 10:29:32 +0800

    feat: Logged in users are not allowed to stay on the login page

--- apps/web/src/pages/auth/views/login/index.tsx
+++ apps/web/src/pages/auth/views/login/index.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm, Controller, type SubmitHandler } from 'react-hook-form';
 import { Paper, Typography, Button } from '@mui/material';
+import cls from 'classnames';
+import { useRequest } from 'ahooks';
 import { oauthClientID, oauthClientSecret } from '@milesight/shared/src/config';
 import { useI18n } from '@milesight/shared/src/hooks';
 import { Logo } from '@milesight/shared/src/components';
 import { iotLocalStorage, TOKEN_CACHE_KEY } from '@milesight/shared/src/utils/storage';
+import { useUserStore } from '@/stores';
 import { globalAPI, awaitWrap, isRequestSuccess, getResponseData } from '@/services/http';
 import useFormItems, { type FormDataProps } from '../useFormItems';
 import './style.less';
@@ -12,6 +16,8 @@ import './style.less';
 export default () => {
     const navigate = useNavigate();
     const { getIntlText } = useI18n();
+
+    // ---------- 表单相关处理逻辑 ----------
     const { handleSubmit, control } = useForm<FormDataProps>();
     const formItems = useFormItems({ mode: 'login' });
 
@@ -37,10 +43,35 @@ export default () => {
         iotLocalStorage.setItem(TOKEN_CACHE_KEY, result);
     };
 
+    // ---------- 判断是否已登录 ----------
+    const setUserInfo = useUserStore(state => state.setUserInfo);
+    const [loading, setLoading] = useState<boolean>();
+    const token = iotLocalStorage.getItem(TOKEN_CACHE_KEY);
+    useRequest(
+        async () => {
+            if (!token) {
+                setLoading(false);
+                return;
+            }
+
+            setLoading(true);
+            const [error, resp] = await awaitWrap(globalAPI.getUserInfo());
+            setLoading(false);
+            if (error || !isRequestSuccess(resp)) return;
+
+            setUserInfo(getResponseData(resp));
+            navigate('/', { replace: true });
+        },
+        { debounceWait: 300 },
+    );
+
     return (
         <div className="ms-view-login">
             <Logo />
-            <Paper className="ms-auth-container" elevation={3}>
+            <Paper
+                className={cls('ms-auth-container', { hidden: loading !== false })}
+                elevation={3}
+            >
                 <Typography variant="h5" align="center">
                     {getIntlText('common.document.title')}
                 </Typography>
